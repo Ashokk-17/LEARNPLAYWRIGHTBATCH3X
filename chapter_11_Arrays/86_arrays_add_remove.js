b3bdd610b70da7be188101e472cb63ccbef8a8ae
Ashok K <ashok.7406625107@gmail.com>
2026-07-07 17:46:57 +0530

    Fill in chapter_11 array exercises (add/remove, splice, real-world example)

---
+++ chapter_11_Arrays/86_arrays_add_remove.js
@@ -0,0 +1,22 @@
+let arr = [1,2,3,4];
+console.log(arr);
+
+arr.push(5);
+console.log(arr);
+
+arr.pop();
+console.log(arr);
+
+arr.unshift(0);
+console.log(arr);
+arr.unshift(20);
+console.log(arr);
+
+arr.shift();
+console.log(arr);
+
+console.log(arr);
+arr.unshift(100);
+console.log(arr);
+arr.shift();
+console.log(arr);
